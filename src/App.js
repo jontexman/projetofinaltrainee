6c495472d84cb64d1b3aabfc703236bc4a0ae85f
Pedro Lucas <pedroluksteixeira@gmail.com>
2021-06-26 16:36:34 -0300

    Excluindo Arquivos/Limpeza da aplicação

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import { Footer } from './Components/Footer'
+import { Footer } from './Components/Footer';
+import { Navbar } from './Components/Navbar';
 
 function App() {
   return (
-    <Footer />
+    <>
+      <Navbar />
+      <Footer />
+    </>
   );
 }
 

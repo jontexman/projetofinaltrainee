75c58c30df0a42b0cc1d6ce754fe429831b3bd28
jontexman <jpmdoliveira@gmail.com>
2021-07-04 02:47:00 -0300

    login fixed, works now

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <Router>
-    <Auth0ProviderWithHistory>
-      <App />
-    </Auth0ProviderWithHistory>
-  </Router>,
+  <Auth0Provider
+    domain="dev-5iv1ev1a.us.auth0.com"
+    clientId="kikgiANBDbmc7oc3QM2hL6tKHqTnWnlz"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
   document.getElementById("root")
 );
